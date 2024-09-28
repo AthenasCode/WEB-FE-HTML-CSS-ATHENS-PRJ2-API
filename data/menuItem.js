f2ffdc38f1e287d6e01f794447c326c41218f68d
torrober <robemario124@gmail.com>
2024-09-28 17:21:39 -0500

    menu items

---
+++ data/menuItem.js
@@ -0,0 +1,42 @@
+const menuItems = [
+    {
+
+        title: "Tecnología",
+        items: [
+            { label: "Computadores", link: "/PLP?category=computadores" },
+            { label: "Televisores", link: "/PLP?category=televisores" },
+            { label: "Audio", link: "/PLP?category=audio" },
+            { label: "Video", link: "/PLP?category=video" },
+            { label: "Impresión", link: "/PLP?category=impresion" },
+            { label: "Cámaras", link: "/PLP?category=camaras" }
+        ]
+    },
+    {
+        title: "Electrodomésticos",
+        items: [
+            { label: "Climatización", link: "/PLP?category=climatizacion" },
+            { label: "Refrigeración", link: "/PLP?category=refrigeracion" },
+            { label: "Lavadoras/Secadoras", link: "/PLP?category=lavadoras" }
+        ]
+    },
+    {
+        title: "Celulares",
+        items: [
+            { label: "Smartwatch", link: "/PLP?category=smartwatch" },
+            { label: "Tabletas", link: "/PLP?category=tabletas" },
+            { label: "Celulares", link: "/PLP?category=celulares" }
+        ]
+    },
+    {
+        title: "Hogar",
+        items: [
+            { label: "Salas", link: "/PLP?category=salas" },
+            { label: "Comedor", link: "/PLP?category=comedor" },
+            { label: "Cocina", link: "/PLP?category=cocina" },
+            { label: "Baño", link: "/PLP?category=baño" }
+        ]
+    }
+
+];
+
+module.exports = menuItems;
--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const productsPLP = require("./data/productsPLP");
 const linksbread = require("./data/linksbread");
 const adImage = require("./data/adImage");
+const menuItems = require("./data/menuItem");
 module.exports = () => {
   return {
     adImage,
     linksbread,
     productsPLP,
+    menuItems,
   };
 };
